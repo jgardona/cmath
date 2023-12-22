397e95d4af7e63a26862b92c3fc2cd9882ed8b30
jgardona <jcbritobr@gmail.com>
2023-12-22 16:10:44 -0300

    Documentation of histogram done.

--- histogram/histogram.go
+++ histogram/histogram.go
@@ -12,6 +12,8 @@ import (
 	"github.com/jgardona/cmath/statistics"
 )
 
+// A histogram for discrete random values, with mean,
+// standard deviation, median, min, max and total count values.
 type Histogram struct {
 	values []int
 	mean   float64
@@ -22,44 +24,55 @@ type Histogram struct {
 	total  int
 }
 
+// NewHistogram instantiates a histogram given an int array.
 func NewHistogram(histogram []int) *Histogram {
 	data := &Histogram{values: histogram}
 	data.Update()
 	return data
 }
 
+// Values returns the histogram array.
 func (h Histogram) Values() []int {
 	return h.values
 }
 
+// Mean returns the histogram mean.
 func (h Histogram) Mean() float64 {
 	return h.mean
 }
 
+// StdDev returns the histogram's standard deviation.
 func (h Histogram) StdDev() float64 {
 	return h.stddev
 }
 
+// Median returns the histogram's median.
 func (h Histogram) Median() int {
 	return h.median
 }
 
+// Min returns the histogram's minimum value.
 func (h Histogram) Min() int {
 	return h.min
 }
 
+// Max returns the histogram's maximum value.
 func (h Histogram) Max() int {
 	return h.max
 }
 
+// Total returns the histogram's total count.
 func (h Histogram) Total() int {
 	return h.total
 }
 
+// Range returns an int range around the mean, given the percent.
 func (h Histogram) Range(percent float64) ranges.Range[int] {
 	return statistics.Range(h.values, percent)
 }
 
+// Update updates all values in histogram. This function must only
+// be called to recalculate the histogram.
 func (h *Histogram) Update() {
 	var i int
 	var n int = len(h.values)
