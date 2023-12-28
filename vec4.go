eff7e4db222ce50f5c89153ecec943754c08bd7e
jgardona <jcbritobr@gmail.com>
2023-12-28 11:10:09 -0300

    Beginning vec4.

--- vec4.go
+++ vec4.go
@@ -1,4 +1,32 @@
 package cmath
 
 type Vec4 struct {
+	x float64
+	y float64
+	z float64
+	w float64
+}
+
+func NewVec4(x, y, z, w float64) Vec4 {
+	panic("not yet implemented")
+}
+
+func (v Vec4) X() float64 {
+	return v.x
+}
+
+func (v Vec4) Min() float64 {
+	panic("not yet implemented")
+}
+
+func (v Vec4) Y() float64 {
+	return v.y
+}
+
+func (v Vec4) Z() float64 {
+	return v.z
+}
+
+func (v Vec4) W() float64 {
+	return v.w
 }
